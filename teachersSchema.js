39bd4665f6905f4e2820d2edc33a32f62bc66079
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-03-25 16:30:22 +0100

    terminado

--- teachersSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const teachersSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  groups: [String],
-});
-
-module.exports = mongoose.model('Teachers', teachersSchema);
